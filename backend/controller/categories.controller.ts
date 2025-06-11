ff0db6844e7a2ae9f690389387a8b9255818bc39
n0aHVN <superherodung123@gmail.com>
2025-06-11 16:54:04 +0700

    update Add Category for controller

--- backend/controller/categories.controller.ts
+++ backend/controller/categories.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CategoryDto } from "../dto/article-category.dto";
+import type { Category } from "../dto/category.validator";
 import { InternalError } from "../error/internal.error";
 import pool from "../src/db-pool";
 
@@ -7,9 +7,22 @@ export class CategoriesController{
     static async getAllCategories(req: Request, res: Response) {
         try {
             const result = await pool.query('SELECT * FROM categories ORDER BY id');
-            res.json(result.rows as CategoryDto[]);
+            res.status(201).json(result.rows as Category[]);
         } catch (err: any) {
             throw new InternalError("Cannot Get Categories")
+        }
+    }
+    
+    static async addCategory(req: Request, res: Response) {
+        try {
+            const { name, slug } = req.body;
+            const result = await pool.query(
+                'INSERT INTO categories (name, slug) VALUES ($1, $2) RETURNING *',
+                [name, slug]
+            );
+            res.status(201).json(result.rows[0]);
+        } catch (err: any) {
+            throw new InternalError("Cannot Add Category");
         }
     }
 }
