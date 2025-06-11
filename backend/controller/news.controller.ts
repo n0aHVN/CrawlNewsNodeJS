5e15e469c3d82b96245b7f6308c4118ca52bb435
n0aHVN <superherodung123@gmail.com>
2025-06-11 16:54:34 +0700

    Update news.controller.ts

--- backend/controller/news.controller.ts
+++ backend/controller/news.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import pool from '../src/db-pool';
-import { CategoryDto, ArticleDto } from '../dto/article-category.dto';
+
 import { InternalError } from '../error/internal.error';
 import { NotFoundError } from '../error/not-found.error';
+import { Article } from '../dto/article.validator';
 
 export class NewsController {
     static async getAllArticles(req: Request, res: Response) {
         try {
             const result = await pool.query('SELECT * FROM articles ORDER BY published_at DESC');
-            res.json(result.rows as ArticleDto[]);
+            res.json(result.rows as Article[]);
         } catch (err: any) {
             throw new InternalError("Cannot Get Articles")
         }
@@ -21,7 +22,7 @@ export class NewsController {
             if (result.rows.length === 0) {
                 throw new NotFoundError();
             }
-            res.json(result.rows[0] as ArticleDto);
+            res.json(result.rows[0] as Article);
         } catch (err: any) {
             throw new InternalError("Cannot Get Articles")
         }
@@ -36,7 +37,7 @@ export class NewsController {
                  RETURNING *`,
                 [title, content, slug, category_id, published_at]
             );
-            res.status(201).json(result.rows[0] as ArticleDto);
+            res.status(201).json(result.rows[0] as Article);
         } catch (err: any) {
             throw new InternalError("Cannot Add Article")
         }
