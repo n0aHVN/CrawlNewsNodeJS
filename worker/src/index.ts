1216fa73fd4e4b42d76155cd9f56b6436cbbc4c6
n0aHVN <superherodung123@gmail.com>
2025-06-18 14:35:37 +0700

    Add cronjob
    Cronjob run for every 1 hour

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -2,6 +2,7 @@ import { app } from "./app";
 import { crawlCategories } from "./job/crawl-categories.job";
 import dotenv from 'dotenv';
 import { crawlNewLinks, crawlNewsContent } from "./job/crawl-news.job";
+import cron from "node-cron";
 dotenv.config();
 
 const start = async () =>{
@@ -11,3 +12,9 @@ const start = async () =>{
 };
 
 start();
+
+// Schedule to run every 1 hour
+cron.schedule('0 * * * *', async () => {
+    console.log('Running crawling jobs (scheduled)...');
+    await start();
+});
